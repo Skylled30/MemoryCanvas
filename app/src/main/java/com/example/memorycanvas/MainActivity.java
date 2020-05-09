16343db66d98b7af443fc77924be1687ddf6b305
Alexander <vanteev@mail.ru>
2020-05-10 01:27:56 +0800

    Finish project

--- app/src/main/java/com/example/memorycanvas/MainActivity.java
+++ app/src/main/java/com/example/memorycanvas/MainActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TilesView view;
+    Button button;
+    boolean start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         view = findViewById(R.id.view);
+        button = findViewById(R.id.button2);
+        start = true;
     }
 
-    public void onNewGameClick(View v) {
-        view.newGame(); // запустить игру заново
-
+    public void turnOnGame(View v){
+        if(start) {
+            view.turnOnGame();
+            button.setText("Play again");
+            start = false;
+        } else{
+            view.newGame();
+            Toast.makeText(this, "New game is started", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/memorycanvas/TilesView.java
+++ app/src/main/java/com/example/memorycanvas/TilesView.java
@@ -7,12 +7,15 @@ import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 class Card {
     Paint p = new Paint();
@@ -41,18 +44,31 @@ class Card {
             return true;
         } else return false;
     }
-
 }
 
 public class TilesView extends View {
     // пауза для запоминания карт
-    final int PAUSE_LENGTH = 2; // в секундах
+    final int PAUSE_LENGTH = 1; // в секундах
     boolean isOnPauseNow = false;
 
     // число открытых карт
     int openedCard = 0;
+    // количество карт
+    int count_cards_row = 4;
+    int count_cards_col = 2;
+    // массив цветов
+    ArrayList<Integer> colors;
+    // использованные цвета
+    SparseIntArray used_color;
+    // количество цветов
+    int amountColors;
+    boolean gameOn = false;
+
+    boolean check = true;
+    Random random = new Random();
 
     ArrayList<Card> cards = new ArrayList<>();
+    ArrayList<Card> openCards = new ArrayList<>();
 
     int width, height; // ширина и высота канвы
 
@@ -65,23 +81,41 @@ public class TilesView extends View {
         // 1) заполнить массив tiles случайными цветами
         // сгенерировать поле 2*n карт, при этом
         // должно быть ровно n пар карт разных цветов
-        cards.add(new Card(0,0, 200, 150, Color.YELLOW));
-        cards.add(new Card(200+50, 0, 200 + 200 + 50, 150, Color.YELLOW));
+        colors = new ArrayList<>();
+        used_color = new SparseIntArray();
+        colors.add(Color.MAGENTA);
+        colors.add(Color.RED);
+        colors.add(Color.GREEN);
+        colors.add(Color.YELLOW);
+//        colors.add(Color.WHITE);
+//        colors.add(Color.GRAY);
+//        colors.add(Color.BLACK);
+//        colors.add(Color.CYAN);
+        amountColors = colors.size();
+    }
 
-        cards.add(new Card(0,200, 200, 150 + 200, Color.RED));
-        cards.add(new Card(200+50, 200, 200 + 200 + 50, 150+200, Color.RED));
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        width = getWidth();
+        height = getHeight();
+        // инициализация карт
+        if(check){
+            initCards();
+            check = false;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        width = canvas.getWidth();
-        height = canvas.getHeight();
         // 2) отрисовка плиток
         // задать цвет можно, используя кисть
         Paint p = new Paint();
-        for (Card c: cards) {
-            c.draw(canvas);
+        if(gameOn) {
+            for (Card c : cards) {
+                c.draw(canvas);
+            }
         }
     }
 
@@ -94,11 +128,12 @@ public class TilesView extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN && !isOnPauseNow)
         {
             // палец коснулся экрана
-
             for (Card c: cards) {
 
                 if (openedCard == 0) {
                     if (c.flip(x, y)) {
+                        openCards.add(c);
+                        Log.d("mytag", "1 card = " + c.color + " size = " + openCards.size());
                         Log.d("mytag", "card flipped: " + openedCard);
                         openedCard ++;
                         invalidate();
@@ -107,36 +142,71 @@ public class TilesView extends View {
                 }
 
                 if (openedCard == 1) {
-
-
                     // перевернуть карту с задержкой
                     if (c.flip(x, y)) {
                         openedCard ++;
+                        openCards.add(c);
                         // 1) если открылис карты одинакового цвета, удалить их из списка
                         // например написать функцию, checkOpenCardsEqual
 
                         // 2) проверить, остались ли ещё карты
                         // иначе сообщить об окончании игры
-
-                        // если карты открыты разного цвета - запустить задержку
                         invalidate();
+                        // если карты открыты разного цвета - запустить задержку
                         PauseTask task = new PauseTask();
                         task.execute(PAUSE_LENGTH);
                         isOnPauseNow = true;
                         return true;
                     }
                 }
-
             }
         }
-
-
          // заставляет экран перерисоваться
         return true;
     }
 
+    public void initCards(){
+        used_color.clear();
+        for (int i = 0; i < amountColors; i++) {
+            used_color.put(colors.get(i), 0);
+        }
+        float card_width = width / count_cards_col;
+        float card_height = height / count_cards_row;
+        int padding = 10;
+        float width_new = card_width - (padding * 2);
+        float height_new  = card_height - (padding * 2);
+        for(int i = 1; i < count_cards_row + 1; i++) {
+            for (int j = 1; j < count_cards_col + 1; j++) {
+                cards.add(new Card(padding + (card_width * j - card_width), card_height * i - card_height + padding, width_new, height_new, getColor()));
+            }
+        }
+    }
+
+    public int getColor(){
+        int color_number = random.nextInt(amountColors);
+        int color = colors.get(color_number);
+        while(used_color.get(color) == 2) {
+            color_number = random.nextInt(amountColors);
+            color = colors.get(color_number);
+        }
+        used_color.put(color, used_color.get(color) + 1);
+        return color;
+    }
+
     public void newGame() {
         // запуск новой игры
+        cards.clear();
+        initCards();
+        invalidate();
+    }
+
+    public void turnOnGame(){
+        gameOn = true;
+        invalidate();
+    }
+
+    public void gameOver(){
+        Toast.makeText(getContext(), "You win", Toast.LENGTH_SHORT).show();
     }
 
     class PauseTask extends AsyncTask<Integer, Void, Void> {
@@ -160,8 +230,16 @@ public class TilesView extends View {
                     c.isOpen = false;
                 }
             }
+            if(openCards.get(0).color == openCards.get(1).color){
+                cards.remove(openCards.get(0));
+                cards.remove(openCards.get(1));
+            }
             openedCard = 0;
             isOnPauseNow = false;
+            openCards.clear();
+            if(cards.size() == 0){
+                gameOver();
+            }
             invalidate();
         }
     }
